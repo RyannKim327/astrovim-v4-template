d01249fe76cda6aef68045c1b4b3c3cabb4d9372
RyannKim327 <rksesgundo123@gmail.com>
2024-04-21 05:42:53 +0800

    04-21-24 05:42

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -15,7 +15,7 @@ return {
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
       init = { -- this table overrides highlights in all themes
-        Normal = { bg = "#000000" },
+        Normal = { bg = "#00000000" },
       },
       astrotheme = { -- a table of overrides/changes when applying the astrotheme theme
         Normal = { bg = "#00000000" },
