6367bd1c5aa958eb4160f362e4cda77a1fe66017
RyannKim327 <rksesgundo123@gmail.com>
2024-04-21 08:49:33 +0800

    04-21-24 08:49 [Added NVIM Silicon]

---
+++ lua/plugins/nvim-silicon.lua
@@ -0,0 +1,21 @@
+return {
+	"michaelrommel/nvim-silicon",
+	lazy = true,
+	cmd = "Silicon",
+	init = function()
+		local wk = require "which-key"
+		wk.register({
+			["<leader>sc"] = { ":Silicon<CR>", "Snapshot Code" },
+		}, { mode = "v" })
+	end,
+	config = function()
+	require("silicon").setup {
+		font = "CaskaydiaCove Nerd Font Mono=34;Noto Color Emoji=34",
+		to_clipboard = true,
+		window_title = function()
+		return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()), ":t")
+		end,
+		theme = "TwoDark",
+	}
+	end,
+}
